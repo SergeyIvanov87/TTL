c59a4e3d4977b9a8803a1f3bb2bcf2a297714a8d
SergeyIvanov87 <sergius140608@rambler.ru>
2025-11-08 23:48:39 +0100

    remove RW examples completely

--- examples/ResourcesFramework/ResourceFrameworkSpecs.h
+++
@@ -1,8 +0,0 @@
-#ifndef RESOURCES_FRAMEWORK_SPECS_H
-#define RESOURCES_FRAMEWORK_SPECS_H
-
-#include <ttl/include/ResourcesFramework/ResourcesFramework.h>
-#include "CustomResourceLoaders.h"
-using ResourcesFramework = Resources::ResourcesFrameworkFactory<ResourceA, ResourceB>;
-
-#endif //RESOURCES_FRAMEWORK_SPECS_H
